ed2ae85e0ff0f8d3891b69bd4d14892447211274
Alexandr <31060406+Sanier@users.noreply.github.com>
2021-12-13 16:43:43 +0300

    Add files via upload

---
+++ Practice.Tests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Practice.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Additional(250, 50) == 300);
+        }
+
+        [Fact]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Fact]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Miltiplication(5, 5) == 25);
+        }
+
+        [Fact]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Division(200, 10) == 20);
+        }
+
+        [Fact]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
